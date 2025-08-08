d5724ef5c6e49cc116069109da068442fdffa102
Alex Rivas <alexrb.dev@gmail.com>
2025-08-08 10:26:45 +0200

    Retomando Proyecto

--- gestor-compras/src/App.js
+++ gestor-compras/src/App.js
@@ -4,13 +4,9 @@ import Login from './components/Auth/Login';
 import Home from './components/Home/Home';
 import ProductCatalog from './components/Products/ProductCatalog';
 import OrderCreate from './components/Orders/OrderCreate';
-import OrderSupplierCreate from './components/Orders/OrderSupplierCreate';
 import OrderReview from './components/Orders/OrderReview';
-import OrderSupplierReview from './components/Orders/OrderSupplierReview';
 import OrderList from './components/Orders/OrderList';
-import OrderSupplierList from './components/Orders/OrderSupplierList';
 import OrderDetail from './components/Orders/OrderDetail';
-import OrderSupplierDetail from './components/Orders/OrderSupplierDetail';
 import ProtectedRoute from './components/Layout/ProtectedRoute';
 import Navbar from './components/Layout/Navbar';
 import { AuthProvider } from './context/AuthContext';
@@ -31,13 +27,9 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/catalogo" element={<ProductCatalog />} />
               <Route path="/crear-pedido" element={<OrderCreate />} />
-              <Route path="/crear-pedido-proveedor" element={<OrderSupplierCreate />} />
               <Route path="/revisar-pedido" element={<OrderReview />} />
-              <Route path="/revisar-pedido-proveedor" element={<OrderSupplierReview />} />
               <Route path="/mis-pedidos" element={<OrderList />} />
-              <Route path="/mis-pedidos-proveedor" element={<OrderSupplierList />} />
               <Route path="/mis-pedidos/:orderId" element={<OrderDetail />} />
-              <Route path="/mis-pedidos-proveedor/:orderId" element={<OrderSupplierDetail />} />
               <Route path="/editar-pedido/:orderId" element={<OrderEdit />} />
             </Route>
 
